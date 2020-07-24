0d30fda7045c6ab3d526045c68ae09e5c275afe2
Ade Yahya <adeyahyaprasetyo@gmail.com>
2020-07-25 00:08:20 +0700

    create pokemon detail page

---
+++ src/pages/pokemon/[pid].tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { FunctionComponent } from 'react';
+import { useRouter } from 'next/dist/client/router';
+import { useQuery } from '@apollo/react-hooks';
+import { getPokemonQuery } from '../../queries/getPokemon.query';
+import { getPokemon, getPokemonVariables } from '../../queries/types/getPokemon';
+import { PokemonDetailCard } from '../../components/PokemonDetailCard';
+import { PokemonFragment } from '../../queries/types/PokemonFragment';
+import { getPokemonList, getPokemonListVariables } from '../../queries/types/getPokemonList';
+import { getPokemonListQuery } from '../../queries/getPokemonList.query';
+import { PokemonListCard } from '../../components/PokemonListCard';
+
+const PAGINATION_PAGE_SIZE = 12;
+
+const PokemonDetailPage: FunctionComponent = () => {
+  const router = useRouter();
+  const id = String(router.query?.pid) || 'unknown';
+  const { data, loading } = useQuery<getPokemon, getPokemonVariables>(getPokemonQuery, {
+    variables: {
+      id,
+    },
+  });
+
+  const { data: relatedPokemonData, loading: relatedPokemonLoading } = useQuery<
+    getPokemonList,
+    getPokemonListVariables
+  >(getPokemonListQuery, {
+    variables: {
+      first: PAGINATION_PAGE_SIZE,
+      after: id,
+    },
+  });
+
+  const pokemon = data?.pokemon;
+  const relatedPokemons = relatedPokemonData?.pokemons?.items || [];
+
+  if (loading) return <p>loading</p>;
+
+  return (
+    <div className="px-2 max-w-screen-lg mx-auto">
+      <PokemonDetailCard pokemon={pokemon as PokemonFragment} />
+      <h2 className="text-4xl font-bold px-5 mt-10" style={{ marginBottom: '-30px' }}>
+        Related Pokemon
+      </h2>
+      <PokemonListCard pokemons={relatedPokemons} loading={relatedPokemonLoading} />
+    </div>
+  );
+};
+
+export default PokemonDetailPage;
